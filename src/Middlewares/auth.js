02d7982b2321e4146d02708a04dddf40b0030262
deveckor <deveckor@gmail.com>
2022-03-24 22:38:07 -0600

    add feature-auth

---
+++ src/Middlewares/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('../Lib/jwt');
+
+function auth(req, res, next) {
+    try {
+        const {authorization: token} = req.headers
+
+        const isValidToken = jwt.verify(token)
+
+        if(!isValidToken) throw new Error('Not Authorized')
+        req.officerCurrent = isValidToken.id;
+        next();
+    } catch (error) {
+        res.status(401)
+        res.json({
+            success: false,
+            message: 'not authorized',
+            error: error.message
+        })
+    }
+}
+
+

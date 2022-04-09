7dec1ff9edbd4d3db3e64a47ab4c8becf8d1d1cb
deveckor <deveckor@gmail.com>
2022-04-09 13:13:08 -0500

    add Script to server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const cors = require('cors');
+const office = require('./Routers/office');
+const projects = require('./Routers/projects');
 
 const server = express();
+
+server.use(express.json());
+server.use(cors());
+server.use('/', office);
+server.use('/', projects);
 
 module.exports = server

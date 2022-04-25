f7bf6f44bba64ef6bf172957a854977c6e7caff9
deveckor <deveckor@gmail.com>
2022-04-24 22:54:44 -0500

    add upload

--- src/Middlewares/multer.js
+++
@@ -1,9 +0,0 @@
-const multer = require('multer');
-let upload = multer({dest: 'temp/'});
-
-function upLoad(req,res,next) {
-    upload.single('fileToUpload');
-    next();
-}
-
-module.exports = upLoad;
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const office = require('./Routers/office');
 const projects = require('./Routers/projects');
+const upload = require('./Routers/uploads');
 
 const server = express();
 
@@ -10,6 +11,7 @@ server.use(express.json());
 server.use(cors());
 server.use('/', office);
 server.use('/', projects);
+server.use('/', upload);
 
 
 module.exports = server
